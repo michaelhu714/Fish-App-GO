a54a51f3ccd5cf083a2817f34983a293c8fcd840
Michael Hu <michaelhu714@gmail.com>
2024-12-29 16:57:08 -0500

    first commit

---
+++ cards.go
@@ -0,0 +1,12 @@
+package card 
+
+
+var (
+	cardNumber = [...]string{"2","3","4","5","6","7","8","9","10","J","Q","K","A"}
+	cardSuit = [4]string{'S', 'D', 'H', 'C'}
+)
+
+var (
+	twoThroughSeven = "234567"
+	eightsAndJokers = 
+)
