f65f10b7386958544ec2dbd55c79782a4f681433
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-17 18:45:22 -0500

    feat: adding boilerplate to main.go

--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const portNum string = ":8000"
+
+func main() {
+	err := http.ListenAndServe(portNum, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Server started on port %s\n", portNum)
+}
