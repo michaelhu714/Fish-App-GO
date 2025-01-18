2d8c9029211ccf6b9610feb1b0d69847698944a1
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-17 19:06:07 -0500

    feat: adding test endpoints to go server

--- backend/main.go
+++ backend/main.go
@@ -9,9 +9,11 @@ import (
 const portNum string = ":8000"
 
 func main() {
+	http.HandleFunc("/", nil)
+	http.HandleFunc("/new", nil)
+	fmt.Printf("Server started on port %s\n", portNum)
 	err := http.ListenAndServe(portNum, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Server started on port %s\n", portNum)
 }
