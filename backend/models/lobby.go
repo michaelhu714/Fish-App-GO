aeb05fd72252f26191abb042fb6f5b7b7bcfe1c5
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-03-03 17:16:05 -0500

    sample model in models dir

--- backend/models/lobby.go
+++ backend/models/lobby.go
@@ -1,11 +1,30 @@
 package models
 
-type CreateLobbyRequest struct {
-	Name string `json:"name" validate:"required"`
+type Lobby struct {
+	Id         int
+	Name       string
+	Owner      Player
+	NumPlayers int
+	Players    []Player
 }
 
-type CreateLobbyResponse struct {
+type CreateLobbyReq struct {
+	Name       string `json:"name" validate:"required"`
+	NumPlayers int    `json:"num_players"` // default is 6
+}
+
+type CreateLobbyResp struct {
 	Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Owner Player `json:"owner"`
+}
+
+type JoinLobbyReq struct {
+	Id   int    `json:"id" validate:"required"`
+	Name string `json:"name" validate:"required"`
+}
+
+type JoinLobbyResp struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
